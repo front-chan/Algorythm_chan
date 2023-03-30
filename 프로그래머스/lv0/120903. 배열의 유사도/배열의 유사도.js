47d2c1333272c508d5d849300772ddd01ee573b1
최신영 <114590923+front-chan@users.noreply.github.com>
2023-03-31 04:12:48 +0900

    Update 배열의 유사도.js

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,17 @@
 function solution(s1, s2) {
     var answer = 0;
     
-    for (let i = 0; i < s1.length; i++) {
-        for (let j = 0; j < s2.length; j++) {
-            if (s1[i] === s2[j]) answer++;
+    // s1의 배열을 for문을 적용하고 그 안에 s2의 배열에 for문을 적용하여 두 값을 비교해 같으면 answer에 +1을 한다.
+    for (let a of s1) {
+        for (let b of s2) {
+            if (a === b) answer++
         }
     }
+    // for (let i = 0; i < s1.length; i++) {
+    //     for (let j = 0; j < s2.length; j++) {
+    //         if (s1[i] === s2[j]) answer++;
+    //     }
+    // }
     
     return answer;
 }
