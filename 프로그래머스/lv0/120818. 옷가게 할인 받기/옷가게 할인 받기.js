ace101bfab8b3f91e01df84f52d0c695eb8e7911
최신영 <114590923+front-chan@users.noreply.github.com>
2023-03-31 03:40:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,53 @@
+function solution(price) {
+    // var answer = 0;
+    
+    if (price >= 500000) {
+        return Math.floor(price * 0.8);
+    } else if (price >= 300000) {
+        return Math.floor(price * 0.9);
+    } else if (price >= 100000) {
+        return Math.floor(price * 0.95);
+    } else return price
+    
+    // 코드 실행 되고 테스트는 여전히 실패
+    // if (price >= 500000) {
+    //     answer = price * (1 - (20 / 100));
+    // } else if (price >= 300000) {
+    //     answer = price * (1 - (10 / 100));
+    // } else if (price >= 100000) {
+    //     answer = price * (1 - (5 / 100));
+    // }
+    
+    // 코드 실행은 되지만 테스트는 또 실패
+    // if (price >= 500000) {
+    //     answer = price - (price * 0.2);
+    // } else if (price >= 300000) {
+    //     answer = price - (price * 0.1);
+    // } else if (price >= 100000) {
+    //     answer = price - (price * 0.05);
+    // }
+    
+    // 코드 실행은 되지만 테스트는 실패
+    // if (price >= 100000) {
+    //     answer = price - price * 0.05;
+    // } else if (price >= 300000) {
+    //     answer = price - price * 0.1;
+    // } else if (price >= 500000) {
+    //     answer = price - price * 0.2;
+    // }
+    
+    // 코드 실행조차 안됨
+    // switch (price) {
+    //     case price > 100000 :
+    //         answer = price - price * 0.05;
+    //         break;
+    //     case price > 300000 :
+    //         answer = price - price * 0.1;
+    //         break;
+    //     case price > 500000 :
+    //         answer = price - price * 0.2;
+    //         break;
+    // }
+    
+    // return Math.floor(answer);
+}
