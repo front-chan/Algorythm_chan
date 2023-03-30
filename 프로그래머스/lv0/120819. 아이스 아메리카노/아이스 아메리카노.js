b67fc0eb75e6a92e89c2fef5287b2b7f3613c3b7
최신영 <114590923+front-chan@users.noreply.github.com>
2023-03-31 03:08:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,18 @@
+function solution(money) {
+    // result 값을 담아줄 빈 배열을 생성한다.
+    var answer = [];
+    // 아이스 아메리카노 가격을 상수로 담아준다.
+    const AMERICANO = 5500;
+    
+    // 아이스 아메리카노를 마실 수 있는 잔 수를 money / AMERICANO를 나눠주고 소수 부분을 버리는 Math.trunc를 사용한다. 
+    var count = Math.trunc(money / AMERICANO)
+    // 남는 잔돈을 계산하기 위해 money에서 AMERICANO 가격과 잔 수를 곱한 값을 빼준다.
+    var price = money - (AMERICANO * count)
+    
+    // console.log(Math.trunc(money / AMERICANO))
+    // console.log(money - (AMERICANO * count))
+    
+    answer = [count, price]
+    
+    return answer;
+}
