7674360e930db08713cea6a1eaf6f044fc05cf98
최신영 <114590923+front-chan@users.noreply.github.com>
2023-04-01 01:24:15 +0900

    Update 가위 바위 보.js

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,8 @@
 function solution(rsp) {
     var answer = '';
     
-    var result = [2, 0, 5]
-    
     let rsps = [...rsp]
-    
-    let results = []
-    
+        
     for (let i = 0; i < rsps.length; i++) {
         // console.log(rsps[i])
         if (Number(rsps[i]) === 2) {
@@ -26,3 +22,17 @@ function solution(rsp) {
     
     return answer;
 }
+
+
+/*
+// 코드 실행 되지만 테스트 실패
+function solution(rsp) {
+    var answer = '';
+
+    if (rsp.includes('2')) answer += "0"
+    if (rsp.includes('0')) answer += "5"
+    if (rsp.includes('5')) answer += "2"
+    
+    return answer;
+}
+*/
