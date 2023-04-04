26c51b6bbea3c116d5a465a2d476112a53510d13
최신영 <114590923+front-chan@users.noreply.github.com>
2023-04-05 01:47:30 +0900

    [level 0] Title: 세균 증식, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -0,0 +1,13 @@
+function solution(n, t) {
+    var answer = [];
+    
+    answer.push(n * 1)
+        
+    for (let i = 2; i <= t + 1; i++) {
+        answer.push(answer[answer.length - 1] * 2)
+
+        // console.log(answer)
+    }
+    
+    return answer[answer.length - 1];
+}
